5c77d1aef1a422e505a246e7e68555f7f38f5a28
Sébastien Chopin <seb@chopin.io>
2017-09-25 23:27:53 +0200

    fix: Exported methods

--- test/fixtures/simple/src/hello/hello.routes.js
+++ test/fixtures/simple/src/hello/hello.routes.js
@@ -1,9 +1,18 @@
+const { HttpError } = require('../../../../../')
+
 module.exports = [
 	{
 		method: 'GET',
 		path: '/hello',
 		handler(req, res) {
 			res.json({ foo: 'bar' })
+		}
+	},
+	{
+		method: 'get',
+		path: '/hello-error',
+		handler() {
+			throw new HttpError('hello-error')
 		}
 	}
 ]
