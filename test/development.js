cd1a4fd14aa6acd641e80ebcf1222d5cdf43a882
Sébastien Chopin <seb@chopin.io>
2017-09-28 21:07:54 +0200

    test: Add more tests

--- test/development.js
+++ test/development.js
@@ -1,19 +1,58 @@
 const test = require('ava')
 const { join } = require('path')
 
-const { start, stop } = require('@terrajs/mono-test-utils')
+const { start, stop, $get } = require('@terrajs/mono-test-utils')
 const monoPath = join(__dirname, '..')
 
-test('Works with custom port', async (t) => {
-	const { server, stdout } = await start(join(__dirname, 'fixtures/simple'), { monoPath })
-	const out = stdout.join('\n')
-	t.true(out.includes('Boot foo module'))
-	t.true(out.includes('Init foo module'))
-	t.true(out.includes('Init hello/hello.init.js'))
-	t.true(out.includes('Init http.init.js'))
-	t.true(out.includes('Adding routes from foo module'))
-	t.true(out.includes('Adding routes from hello/hello.routes.js'))
-	t.true(out.includes('Server running on'))
-	// Close server
-	await stop(server)
+let ctx
+
+test('Start the server and log whats happening', async (t) => {
+	ctx = await start(join(__dirname, 'fixtures/simple'), { monoPath })
+	const stdout = ctx.stdout.join('\n')
+	t.true(stdout.includes('Boot foo module'))
+	t.true(stdout.includes('Init foo module'))
+	t.true(stdout.includes('Init hello/hello.init.js'))
+	t.true(stdout.includes('Init http.init.js'))
+	t.true(stdout.includes('Adding routes from foo module'))
+	t.true(stdout.includes('Adding routes from hello/hello.routes.js'))
+	t.true(stdout.includes('Server running on'))
+})
+
+/*
+** Informations routes
+*/
+test('GET /_ => @terrajs/mono', async (t) => {
+	const { statusCode, body, stdout, stderr } = await $get('/_')
+	t.is(statusCode, 200)
+	t.is(body, '@terrajs/mono')
+	t.is(stderr.length, 0)
+	t.is(stdout.length, 1)
+	t.true(stdout[0].includes('GET /_'))
+})
+
+test('GET /_ping => pong', async (t) => {
+	const { statusCode, body, stdout, stderr } = await $get('/_ping')
+	t.is(statusCode, 200)
+	t.is(body, 'pong')
+	t.is(stderr.length, 0)
+	t.is(stdout.length, 1)
+	t.true(stdout[0].includes('GET /_ping'))
+})
+
+test('GET /_routes => 200 with routes', async (t) => {
+	const { statusCode, body } = await $get('/_routes')
+	t.is(statusCode, 200)
+	t.true(Array.isArray(body))
+	t.true(body.length > 0)
+})
+
+test('GET /404 => 404 status code', async (t) => {
+	const { statusCode, body } = await $get('/404')
+	t.is(statusCode, 404)
+	t.is(body.code, 'not-found')
+	t.is(body.context.url, '/404')
+})
+
+test.after('Close the server', async (t) => {
+	await stop(ctx.server)
 })
